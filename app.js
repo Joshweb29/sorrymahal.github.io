d44dbcd0eb31a51be054d9fc9fec6a4bbdcbb25b
Joshweb29 <cavalierkristin8@gmail.com>
2022-04-25 01:41:57 +0800

    sorry

---
+++ app.js
@@ -0,0 +1,16 @@
+var user = document.getElementById('username');
+
+var pass = document.getElementById('password');
+
+function LogInButton() {
+				console.log(user.value);
+				console.log(pass.value);
+				
+				if(user.value=='admin' || pass.value=='123') {
+								/*window.open('./hshs/sorry.html');*/
+								alert('sorry mahal:<')
+								location.href = "./hshs/sorry.html"
+				}else {
+							alert('Wrong username or password please check!!!');
+				}
+}
